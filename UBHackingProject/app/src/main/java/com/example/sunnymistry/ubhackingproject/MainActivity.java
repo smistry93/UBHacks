85d6d9c8075d2161d2412b3e4d11a42b403551d9
Sunny Mistry <sunnymistry@dhcp020-205-124.wireless.buffalo.edu>
2014-11-08 01:09:12 -0500

    about to pull lukes amazing cam coed

--- UBHackingProject/app/src/main/java/com/example/sunnymistry/ubhackingproject/MainActivity.java
+++ UBHackingProject/app/src/main/java/com/example/sunnymistry/ubhackingproject/MainActivity.java
@@ -4,16 +4,47 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.net.Uri;
+import android.content.Intent;
+import android.provider.MediaStore;
 
 public class MainActivity extends Activity {
+
+    private Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
     }
 
+    public void addListenerOnButton() {
+        myButton = (Button)findViewById(R.id.camera_button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+            private Uri fileUri;
+            @Override
+            public void onClick(View view) {
+                //create new Intent
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+                fileUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);  // create a file to save the video
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);  // set the image file name
+
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1); // set the video image quality to high
+
+                // start the Video Capture Intent
+                startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +67,7 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
