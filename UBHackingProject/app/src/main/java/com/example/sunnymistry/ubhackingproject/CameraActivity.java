c2605fae02f429cb54c861554fe885d019dde429
Luke Carey <Luke@dhcp020-208-139.wireless.buffalo.edu>
2014-11-08 01:03:00 -0500

    yeah bitch

--- UBHackingProject/app/src/main/java/com/example/sunnymistry/ubhackingproject/CameraActivity.java
+++ UBHackingProject/app/src/main/java/com/example/sunnymistry/ubhackingproject/CameraActivity.java
@@ -1,41 +1,25 @@
 package com.example.sunnymistry.ubhackingproject;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.*;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
 
-    private Button btnProduct;
+public class CameraActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        addListenerOnButton();
-    }
-    private void addListenerOnButton() {
-        btnProduct = (Button) findViewById(R.id.camera_button);
-        btnProduct.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+        setContentView(R.layout.activity_camera);
 
-            }
-        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_camera, menu);
         return true;
     }
 
